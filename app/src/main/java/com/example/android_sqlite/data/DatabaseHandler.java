ede7f771a6fdb3dccab88047cfc91cd74c92e423
Pritom <pritomsarkar300@gmail.com>
2022-04-02 12:55:27 +0600

    Database class setup

--- app/src/main/java/com/example/android_sqlite/data/DatabaseHandler.java
+++ app/src/main/java/com/example/android_sqlite/data/DatabaseHandler.java
@@ -6,19 +6,42 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.android_sqlite.R;
+import com.example.android_sqlite.util.Util;
+
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(Context context,String name,SQLiteDatabase.CursorFactory factory,int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
+    }
+
+
+ //we create our table
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        //SQL
+        /*
+          create table _name (id,name,phone_number);
+        */
+
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + " TEXT,"
+                + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
+
+        db.execSQL(CREATE_CONTACT_TABLE);  //creating our table
+
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
-    }
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        //Create a table again
+        onCreate(db);
 
     }
 }
